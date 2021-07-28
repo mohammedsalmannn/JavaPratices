487d9e639ce6307467cee646f5d5e8a078f7739c
mohammedsalmannn <rmdsalman001@gmail.com>
2021-07-28 19:44:52 +0530

    Added : Day 3 Programs

---
+++ DAY_3/Starts5.java
@@ -0,0 +1,10 @@
+package javaPratices.DAY_3;
+
+
+import javaPratices.Utility;
+public class Starts5 {
+    public static void main(String[] args) {
+
+        Utility.randomMinMax();
+    }
+}
---
+++ DAY_3/WindChil.java
@@ -0,0 +1,18 @@
+package javaPratices.DAY_3;
+
+import javaPratices.Utility;
+
+public class WindChil {
+    public static void main(String[] args) {
+        Utility utility = new Utility();
+
+        double windspeed, temperatureInFahrenheit = 0, windChill;
+        temperatureInFahrenheit = Double.parseDouble("40");
+
+        windspeed = Double.parseDouble("60");
+
+        windChill = utility.CalculateWindChill(temperatureInFahrenheit, windspeed);
+        System.out.println("By Calculating wind chill is: " + windChill);
+    }
+
+}
