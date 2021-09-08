0288ff9694e6f888979d34e4b62fbc199cff3866
mohammedsalmannn <rmdsalman001@gmail.com>
2021-09-08 07:05:02 +0530

    Day 10 programs

---
+++ DAY_10/MinMax.java
@@ -0,0 +1,10 @@
+package javaPratices.DAY_10;
+
+import javaPratices.MathFunction;
+
+public class MinMax {
+    public static void main(String[] args)
+    {
+        MathFunction.minMax();
+    }
+}
