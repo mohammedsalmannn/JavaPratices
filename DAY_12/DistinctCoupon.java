db7c64569f8cadc45b057c6415d472bff48af229
mohammedsalmannn <rmdsalman001@gmail.com>
2021-09-08 07:07:46 +0530

    Day 12 programs

---
+++ DAY_12/DistinctCoupon.java
@@ -0,0 +1,20 @@
+package javaPratices.DAY_12;
+
+import javaPratices.Utility;
+
+import java.util.Scanner;
+
+public class DistinctCoupon {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Utility utility = new Utility();
+        System.out.println("enter number for generate coupons");
+        int range = scanner.nextInt();
+        int count=utility.distinctCoupon(range);
+        System.out.println(count + " N times loop runs for finding distinct");
+        scanner.close();
+
+    }
+
+}
