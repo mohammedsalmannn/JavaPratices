05fe06cace8f90b0ea1f5d2642d40acd0535c6c7
mohammedsalmannn <rmdsalman001@gmail.com>
2021-10-20 07:42:14 +0530

    Day 22  programs

---
+++ Day_22/DistinctCoupan.java
@@ -0,0 +1,18 @@
+package javaPratices.Day_22;
+
+
+import javaPratices.Utility;
+
+import java.util.Scanner;
+
+public class DistinctCoupan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Utility utility = new Utility();
+        System.out.println("enter number for generate coupons");
+        int range = scanner.nextInt();
+        int count = utility.distinctCoupon(range);
+        System.out.println(count + " N times loop runs for finding distinct");
+        scanner.close();
+    }
+}
